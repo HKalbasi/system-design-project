8a49e31e36c409c11432fee0bfc16324378aa33d
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-06-05 22:12:16 +0430

    fix django rest web interface login

--- backend/settings.py
+++ backend/settings.py
@@ -131,8 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
     'PAGE_SIZE': 10
 }
